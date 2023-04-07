486f074c4977d31d581c67644be7d580e2c9512d
alpachinof <m.chahkandi2@gmail.com>
2023-04-07 18:13:53 +0330

    fixed support button

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -73,7 +73,7 @@
                   class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
                   @click="success2 = false"
                 >
-                  اره
+                  حمایت مالی
                 </button>
                 <button
                   type="button"
